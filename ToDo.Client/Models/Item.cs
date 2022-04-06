c688769014351c88b1375b12c051e01d800fc611
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-04-06 12:06:35 +0200

    Add Item

---
+++ ToDo.Client/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿namespace ToDo.Client.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public bool Completed { get; set; }
+
+        public Item()
+        {
+            Id = Guid.NewGuid().ToString("n");
+            Text = "";
+        }
+    }
+}
